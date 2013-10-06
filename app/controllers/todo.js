01efbdfafcf2a88f1f9a12f8f2afe892b30fb1a9
Stefan Penner <stefan.penner@gmail.com>
2013-10-06 18:33:33 -0400

    safe CP setter is by checking arguments length

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -3,14 +3,14 @@ var TodoController = Ember.ObjectController.extend({
   isCompleted: function(key, value){
     var model = this.get('model');
 
-    if (value === undefined) {
-      // property being used as a getter
-      return model.get('isCompleted');
-    } else {
+    if (arguments.length === 2) {
       // property being used as a setter
       model.set('isCompleted', value);
       model.save();
       return value;
+   } else {
+      // property being used as a getter
+      return model.get('isCompleted');
     }
   }.property('model.isCompleted'),
 
