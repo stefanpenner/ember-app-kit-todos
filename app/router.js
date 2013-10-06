c0705e5779dae8d8b703cd4d0312d329768da18e
pierre-arnaud lumalé <kadactivity@gmail.com>
2013-10-06 17:12:09 +0200

    Adding the First Route and Template

--- app/router.js
+++ app/router.js
@@ -2,9 +2,8 @@ var Router = Ember.Router.extend(); // ensure we don't share routes between all 
 
 Router.map(function(){
   this.route('component-test');
-  // this.resource('posts', function() {
-  //   this.route('new');
-  // });
+  
+  this.resource('todos', { path: "/" });
 });
 
 export default Router;
