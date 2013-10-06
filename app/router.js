1ebff1e3900c63f7f62360d0ee12c6314d5bd110
Stefan Penner <stefan.penner@gmail.com>
2013-10-06 13:02:01 -0400

    remove cruft

--- app/router.js
+++ app/router.js
@@ -1,8 +1,6 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function(){
-  this.route('component-test');
-  
   this.resource('todos', { path: "/" }, function() {
     // additional child routes
     this.route('active');
