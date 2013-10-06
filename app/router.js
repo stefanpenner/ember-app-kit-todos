1047179b1c5159b8933416ea8b9bd3881f18ad1f
pierre-arnaud lumalé <kadactivity@gmail.com>
2013-10-06 17:56:05 +0200

    Transitioning to Show Only Complete Todos

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ Router.map(function(){
   this.resource('todos', { path: "/" }, function() {
     // additional child routes
     this.route('active');
+    this.route('completed');
   });
 });
 
---
+++ app/routes/todos/completed.js
@@ -0,0 +1,13 @@
+// routes/todos/completed.js
+var TodosCompletedRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function (todo) {
+      return todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller){
+    this.render('todos/index', {controller: controller});
+  }
+});
+
+export default TodosCompletedRoute;
