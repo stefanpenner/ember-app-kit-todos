29da8bc894579c4c876565891828385699cd920f
Stefan Penner <stefan.penner@gmail.com>
2013-11-07 00:43:12 -0500

    test todo creation

--- tests/acceptance/todos_test.js
+++ tests/acceptance/todos_test.js
@@ -49,6 +49,17 @@ function mock(options) {
   return Ember.$.extend(true, {}, options);
 }
 
+function insertNewline(selector) {
+  // should be improved, and likely made an official helper
+  var id = $(selector).attr('id');
+  var view = Ember.View.views[id];
+  if (view) {
+      view.insertNewline(new Ember.$.Event('kewdown'));
+  } else {
+    throw new Error('Could not insertNewline on: `' + selector + '`, because it was not found, or was not an ember-view');
+  }
+}
+
 test('todos renders', function(){
   expect(7);
 
@@ -114,5 +125,17 @@ test('clear completed', function(){
       equal(1, remainingCountText());
       equal(0, completed().length);
     });
+  });
+});
+
+test("create todo", function(){
+  expect(4);
+  visit('/').then(function(){
+    fillIn('#new-todo', 'bro');
+    Ember.run(null, insertNewline, '#new-todo');
+    equal(2, notCompleted().length, 'expected 1 uncompleted');
+    equal(2, remainingCountText());
+    equal(2, completed().length);
+    equal('bro', $('ul#todo-list li label:last').text());
   });
 });
