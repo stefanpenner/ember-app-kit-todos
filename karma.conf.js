88d7d66fb4938358c1680175751b9c06c0424818
Robert Jackson <robert.w.jackson@me.com>
2013-11-30 21:13:38 -0500

    Update to latest EAK (5ded02c).

--- karma.conf.js
+++ karma.conf.js
@@ -6,12 +6,13 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'vendor/almond.js',
+      'vendor/loader.js',
       'vendor/ember-resolver/dist/ember-resolver.js',
       'vendor/jquery/jquery.js',
       'vendor/handlebars/handlebars.js',
       'vendor/ember/ember.js',
-      'vendor/ember-data-shim/ember-data.js',
+      'vendor/ember-data/ember-data.js',
+      'vendor/ic-ajax/main.js',
       'tmp/result/assets/templates.js',
       'tmp/result/assets/app.js',
       'tmp/transpiled/tests/**/*.js',
@@ -31,7 +32,7 @@ module.exports = function(config) {
     ],
 
     preprocessors: {
-      'assets/*.js': 'coverage'
+      'tmp/result/assets/*.js': 'coverage'
     },
 
     // list of files to exclude
